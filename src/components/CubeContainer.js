5813e8cef5a0a53c55b311a9adbbc59953d67716
Jithu Rajan <jithu.tholoor@gmail.com>
2017-02-17 16:54:46 +0530

    Update CubeContainer.js

--- src/components/CubeContainer.js
+++ src/components/CubeContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Cube from './Cube';
 
 class CubeContainer extends Component {
-  render() {
-    return (
-      <div className="cube-container">
-            <Cube/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="cube-container">
+                <Cube translate={[0,0,0]} base="true" orientation={this.props.orientation}/>
+                <Cube translate={[50,0,0]} orientation={this.props.orientation} />
+            </div>
+        );
+    }
 }
 
 export default CubeContainer;
