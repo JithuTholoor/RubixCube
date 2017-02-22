11e16b1b430bd69fab712394df385943d362bf7d
Jithu Rajan <jithu.tholoor@gmail.com>
2017-02-22 19:44:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react';
 import CubeContainer from './CubeContainer';
 
 class App extends Component {
-
-    constructor(props){
-        super(props);
-        this.rangeChange=this.rangeChange.bind(this);
-        this.state={orientation:[0,0,0]};
-    }
-
-    rangeChange(eve) {
-        this.setState({orientation:[0,eve.target.value,0]})
-    }
-
     render() {
         return (
             <div className="app">
-                {this.state.orientation[1]}
-                <input onChange={this.rangeChange} type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
-                <CubeContainer orientation={this.state.orientation}/>
+                <CubeContainer/>
             </div>
         );
     }
