22c83a231ca5d602c032cc619b76867e2dea746e
Jithu Rajan <jithu.tholoor@gmail.com>
2017-02-22 19:45:00 +0530

    Update Cube.js

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,59 +1,14 @@
 import React, { Component } from 'react';
 
 class Cube extends Component {
-
-    toRadians(angle) {
-        return angle * (Math.PI / 180);
-    }
-
-    calcPosition() {
-        if (this.props.orientation) {
-            
-            //z axis calculation.
-            let zAxis = 0;
-            if (this.props.translate[0] != 0 || this.props.translate[2] != 0) {
-                zAxis = this.props.translate[0] == 50 || this.props.translate[2] == -50 ?
-                    Math.sin(this.toRadians(this.props.orientation[1])) * 50 :
-                    Math.sin(this.toRadians(Math.abs(360 - this.props.orientation[1]))) * 50;
-            }
-
-            //x axis calculation
-            let xAxis = 0;
-            if (this.props.translate[0] != 0 || this.props.translate[2] != 0) {
-                let angle = Math.abs(180 - this.props.orientation[1]) / 2;
-                xAxis = Math.abs(zAxis) / Math.tan(this.toRadians(angle)) == Infinity ?
-                 100 : Math.abs(zAxis) / Math.tan(this.toRadians(angle));
-                xAxis = this.props.translate[0] == 50 || this.props.translate[2] == 50 ? xAxis : 0 - xAxis;
-            }
-
-            if (this.props.translate[2] != 0) {
-                if (this.props.translate[2] == 50) {
-                    if (this.props.translate[0] == 0)
-                        return [zAxis, 0, xAxis];
-                    if(this.props.translate[0]==50)
-                        return [-zAxis+ xAxis, 0, xAxis + zAxis];
-                    return [zAxis - xAxis, 0, xAxis + zAxis];
-                }
-                if (this.props.translate[0] == 0)
-                    return [zAxis, 0, xAxis];
-                if (this.props.translate[0] == 50)
-                    return [zAxis + xAxis, 0, -xAxis + zAxis];
-                return [zAxis + xAxis, 0, xAxis - zAxis];
-            }
-            else
-                return [xAxis, 0, zAxis]
-        }
-        return [0, 0, 0];
-    }
-
+    
     cubePosition() {
-        var changedPositions = this.calcPosition();
         return (
             this.props.translate ? {
-                transform: `translate3d(${this.props.translate[0] - changedPositions[0]}px,
-        ${this.props.translate[1] - changedPositions[1]}px,
-        ${this.props.translate[2] - changedPositions[2]}px)
-         rotateX(0deg) rotateY(${this.props.orientation[1]}deg) rotateZ(0deg)`
+                transform: `translate3d(${this.props.translate[0]}px,
+        ${this.props.translate[1]}px,
+        ${this.props.translate[2]}px)
+         rotateX(${this.props.orientation[0]}deg) rotateY(${this.props.orientation[1]}deg) rotateZ(0deg)`
             } : {});
     }
 
