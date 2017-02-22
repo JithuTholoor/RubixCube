e590c4f67346261a1bf05d20706ca8dd039fa436
JiTHu <jithu.tholoor@gmail.com>
2017-02-22 23:08:30 +0530

    Update

--- src/components/CubeContainer.js
+++ src/components/CubeContainer.js
@@ -5,26 +5,32 @@ class CubeContainer extends Component {
 
     constructor(props){
         super(props);
-        this.rangeChange=this.rangeChange.bind(this);
+        this.rangeChangeY=this.rangeChangeY.bind(this);
         this.rangeChangeX=this.rangeChangeX.bind(this);
+        this.rangeChangeZ=this.rangeChangeZ.bind(this);
         this.calcPosition=this.calcPosition.bind(this);
         this.moveCubes=this.moveCubes.bind(this);
-        this.state={positions:[[0, 0, 0],[50, 0, 0]],orientation:[0,0,0],prevOrientation:[0,0,0]};
+        this.state={positions:[[0, 0, 0],[0, -50, 0]],orientation:[0,0,0],prevOrientation:[0,0,0]};
     }
 
-    rangeChange(eve) {
+    rangeChangeY(eve) {
         let currentOrientation=this.state.orientation;
-        this.setState({orientation:[0,eve.target.value,0],prevOrientation:currentOrientation},()=>{
+        this.setState({orientation:[currentOrientation[0],eve.target.value,currentOrientation[2]],prevOrientation:currentOrientation},()=>{
             this.moveCubes();
         });
     }
     rangeChangeX(eve) {
         let currentOrientation=this.state.orientation;
-        this.setState({orientation:[eve.target.value,0,0],prevOrientation:currentOrientation},()=>{
+        this.setState({orientation:[eve.target.value,currentOrientation[1],currentOrientation[2]],prevOrientation:currentOrientation},()=>{
             this.moveCubes();
         });
     }
-
+    rangeChangeZ(eve) {
+        let currentOrientation=this.state.orientation;
+        this.setState({orientation:[currentOrientation[0],currentOrientation[1],eve.target.value],prevOrientation:currentOrientation},()=>{
+            this.moveCubes();
+        });
+    }
     moveCubes(){
         let arr=this.state.positions.slice();
         for(let i=0;i<arr.length;i++){
@@ -38,9 +44,9 @@ class CubeContainer extends Component {
     }
 
     calcPosition(position) {
-        let x = 0;
-        let y = 0;
-        let z = 0;
+        let x = position[0];
+        let y = position[1];
+        let z = position[2];
         if(this.state.prevOrientation[0]!=this.state.orientation[0]) {
             x = position[0];
             y = position[1]* Math.cos(this.toRadians(this.state.orientation[0]-this.state.prevOrientation[0]))-
@@ -52,8 +58,15 @@ class CubeContainer extends Component {
             x = position[0] * Math.cos(this.toRadians(this.state.orientation[1]-this.state.prevOrientation[1])) +
                 position[2] * Math.sin(this.toRadians(this.state.orientation[1]-this.state.prevOrientation[1]));
             y = position[1];
-            z = -position[0] * Math.sin(this.toRadians(this.state.orientation[1]-this.state.prevOrientation[1])) +
+            z = -1*(position[0]* Math.sin(this.toRadians(this.state.orientation[1]-this.state.prevOrientation[1]))) +
                 position[2] * Math.cos(this.toRadians(this.state.orientation[1]-this.state.prevOrientation[1]));
+        }
+        if(this.state.prevOrientation[2]!=this.state.orientation[2]) {
+            x = position[0] * Math.cos(this.toRadians(this.state.orientation[2]-this.state.prevOrientation[2])) -
+                position[1] * Math.sin(this.toRadians(this.state.orientation[2]-this.state.prevOrientation[2]));
+            y = position[0] * Math.sin(this.toRadians(this.state.orientation[2]-this.state.prevOrientation[2])) +
+                position[1] * Math.cos(this.toRadians(this.state.orientation[2]-this.state.prevOrientation[2]));
+            z = position[2];
         }
         return [x, y, z];
     }
@@ -61,9 +74,11 @@ class CubeContainer extends Component {
         return (
             <div>
                 {this.state.orientation[1]}
-                <input onChange={this.rangeChange} value="0" type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
+                <input onChange={this.rangeChangeY} type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
                 {this.state.orientation[0]}
-                <input onChange={this.rangeChangeX} value="0" type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
+                <input onChange={this.rangeChangeX}  type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
+                {this.state.orientation[2]}
+                <input onChange={this.rangeChangeZ}  type="range" min="0" max="360" style={{'margin-bottom':'100px'}}/>
                 <div className="cube-container">
                     <Cube translate={this.state.positions[0]} orientation={this.state.orientation}
                           prevOrientation={this.state.prevOrientation}/>
