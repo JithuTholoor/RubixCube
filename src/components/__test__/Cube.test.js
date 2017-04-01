f4d0b5bd7667320ce18ce81654a4b6faa12bf901
JiTHu <jithu.tholoor@gmail.com>
2017-04-01 10:59:25 +0530

    Updating test cases

--- src/components/__test__/Cube.test.js
+++ src/components/__test__/Cube.test.js
@@ -16,17 +16,33 @@ describe('Cube', () => {
          rotate3d(1,0,0,0deg)`);
     });
 
-    describe('events',()=>{
+    describe('will unmount events',()=>{
         let tree;
         const rotateCubesMock= jest.fn()
         beforeAll(()=>{
             tree = shallow(<Cube rotateCubes={rotateCubesMock} translate={[0,0,0]} orientation={[1,0,0,0]}/>);
         });
+
+        it('willUnmount events umount mouseup event',()=>{
+            tree.unmount();
+            tree.simulate('mousedown');
+            expect(tree.instance()).toBe(null);
+        });
+    })
+
+    describe('mouse events',()=>{
+        let tree;
+        const rotateCubesMock= jest.fn()
+        beforeAll(()=>{
+            tree = shallow(<Cube rotateCubes={rotateCubesMock} translate={[0,0,0]} orientation={[1,0,0,0]}/>);
+        });
+
         it('mouse down',()=>{
             expect(tree.instance().state.touchStarted).toBe(false);
             tree.simulate('mousedown',{stopPropagation:jest.fn()});
             expect(tree.instance().state.touchStarted).toBe(true);
         });
+
         it('mouse move',()=>{
             tree.simulate('mousemove',{clientX:1,clientY:1});
             expect(tree.instance().state.touchStarted).toBe(true);
@@ -34,7 +50,32 @@ describe('Cube', () => {
             expect(rotateCubesMock).toBeCalled();
         });
 
-    })
+        it('mouse up',()=>{
+            tree.instance().onTouchEnd();
+            expect(tree.instance().state.touchStarted).toBe(false);
+        });
+    });
+
+    describe('touch events',()=>{
+        let tree;
+        const rotateCubesMock= jest.fn()
+        beforeAll(()=>{
+            tree = shallow(<Cube rotateCubes={rotateCubesMock} translate={[0,0,0]} orientation={[1,0,0,0]}/>);
+        });
+
+        it('touch start',()=>{
+            expect(tree.instance().state.touchStarted).toBe(false);
+            tree.simulate('touchstart',{stopPropagation:jest.fn()});
+            expect(tree.instance().state.touchStarted).toBe(true);
+        });
+
+        it('touch move',()=>{
+            tree.simulate('touchmove',{stopPropagation:jest.fn(), targetTouches:[{clientX:1,clientY:1}]});
+            expect(tree.instance().state.touchStarted).toBe(true);
+            expect(tree.instance().state.mousePoint).toEqual({x:1,y:1});
+            expect(rotateCubesMock).toBeCalled();
+        });
+    });    
 
 })
 ;
--- src/components/__test__/CubeContainer.test.js
+++ src/components/__test__/CubeContainer.test.js
@@ -2,10 +2,99 @@ import CubeContainer from './../CubeContainer';
 import { render, shallow } from 'enzyme';
 import React, { Component } from 'react';
 
-describe('CubeContainer', () => {    
-    
+describe('CubeContainer', () => {
+
     it('Renders without crashing', () => {
-        const result = render(<CubeContainer/>);
+        const result = render(<CubeContainer />);
         expect(result).toBeDefined();
     });
+
+    describe('mouse events', () => {
+        let tree;
+        beforeAll(() => {
+            tree = shallow(<CubeContainer />);
+        });
+
+        it('mouse down', () => {
+            tree.simulate('mousedown', { stopPropagation: jest.fn(), clientX: 1, clientY: 1 });
+            expect(tree.instance().state.touchStarted).toBe(true);
+            expect(tree.instance().state.mousePoint).toEqual({ x: 1, y: 1 });
+        });
+
+        it('mouse move', () => {
+            tree.simulate('mousemove', { clientX: 1, clientY: 1 });
+            expect(tree.instance().state.touchStarted).toBe(true);
+            expect(tree.instance().state.mousePoint).toEqual({ x: 1, y: 1 });
+        });
+
+        it('mouse up', () => {
+            tree.instance().onTouchEnd();
+            expect(tree.instance().state.touchStarted).toBe(false);
+        });
+    });
+
+    describe('touch events', () => {
+        let tree;
+        beforeAll(() => {
+            tree = shallow(<CubeContainer />);
+        });
+
+        it('touch start', () => {
+            tree.simulate('touchstart', { stopPropagation: jest.fn(), targetTouches: [{ clientX: 1, clientY: 1 }] });
+            expect(tree.instance().state.touchStarted).toBe(true);
+            expect(tree.instance().state.mousePoint).toEqual({ x: 1, y: 1 });
+        });
+
+        it('touch move', () => {
+            tree.simulate('touchmove', { stopPropagation: jest.fn(), targetTouches: [{ clientX: 2, clientY: 2 }] });
+            expect(tree.instance().state.touchStarted).toBe(true);
+            expect(tree.instance().state.mousePoint).toEqual({ x: 2, y: 2 });
+        });
+    });
+
+    describe('Cube space rotation', () => {
+        let tree;
+        beforeAll(() => {
+            tree = shallow(<CubeContainer />);
+        });
+
+        it('rotateCubeSpace - on x move', () => {
+            tree.instance().rotateCubeSpace(1, 0);
+            expect(tree.instance().state.positions[2]).toEqual([49.992384757819565, 0, -0.8726203218641756]);
+            expect(tree.instance().state.angleOfRotation[2]).toEqual(0.9999999999997249);
+            expect(tree.instance().state.rotationVector[2]).toEqual([0, 1.0000000000002751, 0]);
+        });
+
+        it('rotateCubeSpace - on y move', () => {
+            tree.instance().rotateCubeSpace(0, 1);
+            expect(tree.instance().state.positions[2]).toEqual([49.992384757819565, -0.015229324522606748, -0.8724874175625242]);
+            expect(tree.instance().state.angleOfRotation[2]).toEqual(1.414204587376545);
+            expect(tree.instance().state.rotationVector[2]).toEqual([-0.7070933185723947, 0.7070933185723947, -0.0061707099069101745]);
+        });
+    });
+
+    describe('Cube face rotation', () => {
+        let tree;
+        beforeAll(() => {
+            tree = shallow(<CubeContainer />);
+        });
+
+        it('face rotation', () => {
+            tree.instance().rotateCube(0, 1, [1, 0, 0]);
+            expect(tree.instance().state.positions[26]).toEqual([49.11976443595539, 50.86500507968374, 50]);
+        });
+
+        it('face rotation -reverse', () => {
+            tree.instance().rotateCube(0, -1, [1, 0, 0], true);            
+            expect(tree.instance().state.positions[26]).toEqual( [  48.22456651582974, 51.71451618607984, 50 ]);
+        });
+
+        it('face rotation - auto complete', () => {
+            tree.setState({ autoRotation: true });
+            tree.instance().rotateCube(0, 1, [1, 0, 0], true);    
+            expect(tree.instance().state.positions[26]).toEqual( [ 47.31467892558151, 52.54827454987589, 50]);
+        });
+
+    });
+
 });
